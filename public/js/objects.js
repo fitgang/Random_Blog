3733d6bd8dbd39498d7113bc292cc7fbbcfe7bff
Ayush Gupta <70760483+fitgang@users.noreply.github.com>
2021-10-15 20:06:44 +0530

    Add files via upload

---
+++ public/js/objects.js
@@ -0,0 +1,20 @@
+export class Article {
+    constructor(id, title, body, date, author) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.date = date;
+        this.author = author;
+        this.like = false;
+    }
+
+}
+
+export function createArticleList(arr = []) {
+    return {
+        list: arr,
+        addItem: function(item) {
+            this.list.push(item);
+        },
+    };
+}
