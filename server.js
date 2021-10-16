18f9331955e5fe696c206d4d717cb6446d658a81
Ayush Gupta <70760483+fitgang@users.noreply.github.com>
2021-10-16 11:21:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => {})
+app.listen(port, () => { console.log(`listening on ${port}`) });
