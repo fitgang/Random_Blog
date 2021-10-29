54f1a2a5627e55c6aaba16c8e5c42b8e83028993
Ayush Gupta <70760483+fitgang@users.noreply.github.com>
2021-10-29 19:09:22 +0530

    added search feature

--- public/js/article.js
+++ public/js/article.js
@@ -9,37 +9,7 @@
     }
 
     // data to fill the article with different elements used
-    const data = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Videsne ut, quibus summa est in voluptate, perspicuum sit quid iis faciendum sit aut non faciendum? Ita relinquet duas, de quibus etiam atque etiam consideret. <mark>Mihi, inquam, qui te id ipsum rogavi?</mark> Sed plane dicit quod intellegit. Duo Reges: constructio interrete. Traditur, inquit, ab Epicuro ratio neglegendi doloris. <i>Igitur neque stultorum quisquam beatus neque sapientium non beatus.</i> Minime vero istorum quidem, inquit. Illi enim inter se dissentiunt. Ut pompa, ludis atque eius modi spectaculis teneantur ob eamque rem vel famem et sitim perferant? Vos autem cum perspicuis dubia debeatis illustrare, dubiis perspicua conamini tollere. Atque ut ceteri dicere existimantur melius quam facere, sic hi mihi videntur facere melius quam dicere. </p>
-
-    <ul>
-        <li>Omnes enim iucundum motum, quo sensus hilaretur.</li>
-        <li>Videamus igitur sententias eorum, tum ad verba redeamus.</li>
-    </ul>
-
-    <p>Illorum vero ista ipsa quam exilia de virtutis vi! Quam tantam volunt esse, ut beatum per se efficere possit. Idque testamento cavebit is, qui nobis quasi oraculum ediderit nihil post mortem ad nos pertinere? Rem unam praeclarissimam omnium maximeque laudandam, penitus viderent, quonam gaudio complerentur, cum tantopere eius adumbrata opinione laetentur? <i>Et ille ridens: Video, inquit, quid agas;</i> Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Atque ab isto capite fluere necesse est omnem rationem bonorum et malorum. Et tamen ego a philosopho, si afferat eloquentiam, non asperner, si non habeat, non admodum flagitem. Laelius clamores sofòw ille so lebat Edere compellans gumias ex ordine nostros. </p>
-
-    <blockquote cite='http://loripsum.net'>
-        Nam et amici cultus et parentis ei, qui officio fungitur, in eo ipso prodest, quod ita fungi officio in recte factis est, quae sunt orta virtutibus.
-    </blockquote>
-
-    <dl>
-        <dt><dfn>Ita credo.</dfn></dt>
-        <dd>Ac tamen, ne cui loco non videatur esse responsum, pauca etiam nunc dicam ad reliquam orationem tuam.</dd>
-        <dt><dfn>Non igitur bene.</dfn></dt>
-        <dd>Haec quo modo conveniant, non sane intellego.</dd>
-        <dt><dfn>An eiusdem modi?</dfn></dt>
-        <dd>Sed quod proximum fuit non vidit.</dd>
-        <dt><dfn>Stoici scilicet.</dfn></dt>
-        <dd>Verum esto: verbum ipsum voluptatis non habet dignitatem, nec nos fortasse intellegimus.</dd>
-    </dl>
-
-    <p>Et quod est munus, quod opus sapientiae? Curium putes loqui, interdum ita laudat, ut quid praeterea sit bonum neget se posse ne suspicari quidem. Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. Quibus rebus vita consentiens virtutibusque respondens recta et honesta et constans et naturae congruens existimari potest. <a href='http://loripsum.net/' target='_blank'>Itaque hic ipse iam pridem est reiectus;</a> <b>Ego vero volo in virtute vim esse quam maximam;</b> </p>
-
-    <ol>
-        <li>Quod si ita se habeat, non possit beatam praestare vitam sapientia.</li>
-        <li>Semovenda est igitur voluptas, non solum ut recta sequamini, sed etiam ut loqui deceat frugaliter.</li>
-        <li>Ut placet, inquit, etsi enim illud erat aptius, aequum cuique concedere.</li>
-    </ol>`;
+    const data = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Videsne ut, quibus summa est in voluptate, perspicuum sit quid iis faciendum sit aut non faciendum? Ita relinquet duas, de quibus etiam atque etiam consideret. <mark>Mihi, inquam, qui te id ipsum rogavi?</mark> Sed plane dicit quod intellegit. Duo Reges: constructio interrete. Traditur, inquit, ab Epicuro ratio neglegendi doloris. <i>Igitur neque stultorum quisquam beatus neque sapientium non beatus.</i> Minime vero istorum quidem, inquit. Illi enim inter se dissentiunt. Ut pompa, ludis atque eius modi spectaculis teneantur ob eamque rem vel famem et sitim perferant? Vos autem cum perspicuis dubia debeatis illustrare, dubiis perspicua conamini tollere. Atque ut ceteri dicere existimantur melius quam facere, sic hi mihi videntur facere melius quam dicere.</p><ul><li>Omnes enim iucundum motum, quo sensus hilaretur.</li><li>Videamus igitur sententias eorum, tum ad verba redeamus.</li></ul><p>Illorum vero ista ipsa quam exilia de virtutis vi! Quam tantam volunt esse, ut beatum per se efficere possit. Idque testamento cavebit is, qui nobis quasi oraculum ediderit nihil post mortem ad nos pertinere? Rem unam praeclarissimam omnium maximeque laudandam, penitus viderent, quonam gaudio complerentur, cum tantopere eius adumbrata opinione laetentur? <i>Et ille ridens: Video, inquit, quid agas;</i> Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Atque ab isto capite fluere necesse est omnem rationem bonorum et malorum. Et tamen ego a philosopho, si afferat eloquentiam, non asperner, si non habeat, non admodum flagitem. Laelius clamores sofòw ille so lebat Edere compellans gumias ex ordine nostros.</p><blockquote cite='http://loripsum.net'>Nam et amici cultus et parentis ei, qui officio fungitur, in eo ipso prodest, quod ita fungi officio in recte factis est, quae sunt orta virtutibus.</blockquote><dl><dt><dfn>Ita credo.</dfn></dt><dd>Ac tamen, ne cui loco non videatur esse responsum, pauca etiam nunc dicam ad reliquam orationem tuam.</dd><dt><dfn>Non igitur bene.</dfn></dt><dd>Haec quo modo conveniant, non sane intellego.</dd><dt><dfn>An eiusdem modi?</dfn></dt><dd>Sed quod proximum fuit non vidit.</dd><dt><dfn>Stoici scilicet.</dfn></dt><dd>Verum esto: verbum ipsum voluptatis non habet dignitatem, nec nos fortasse intellegimus.</dd></dl><p>Et quod est munus, quod opus sapientiae? Curium putes loqui, interdum ita laudat, ut quid praeterea sit bonum neget se posse ne suspicari quidem. Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. Quibus rebus vita consentiens virtutibusque respondens recta et honesta et constans et naturae congruens existimari potest. <a href='http://loripsum.net/' target='_blank'>Itaque hic ipse iam pridem est reiectus;</a> <b>Ego vero volo in virtute vim esse quam maximam;</b> </p><ol><li>Quod si ita se habeat, non possit beatam praestare vitam sapientia.</li><li>Semovenda est igitur voluptas, non solum ut recta sequamini, sed etiam ut loqui deceat frugaliter.</li><li>Ut placet, inquit, etsi enim illud erat aptius, aequum cuique concedere.</li></ol>";
 
     // DOM elements and other imp data
     const article = JSON.parse(j),
@@ -55,7 +25,7 @@
     image.src = article.author.picture.thumbnail;
     image.alt = authorName;
     name.innerText = authorName;
-    body.innerHTML += data;
+    body.innerHTML = `<p>${article.body}</p>` + data;
 
     removePreloader();
 
@@ -83,7 +53,9 @@ const reactions = document.getElementById("reactions"),
     likeBtn = reactions.querySelector("#like-btn"),
     commentSectionBtn = reactions.querySelector("#comment-btn"),
     shareBtn = reactions.querySelector("#share-btn"),
-    reportBtn = reactions.querySelector("#report-btn");
+    reportBtn = reactions.querySelector("#report-btn"),
+    searchForm = document.getElementById("sna"),
+    searchBar = searchForm.querySelector("input[type='text']");
 
 // add or removes the article form liked articles
 function toggleLike() {
@@ -304,9 +276,80 @@ function openReportDialog() {
     }
 }
 
+function searchQuery(e) {
+    e.preventDefault();
+
+    // On the very first search, put the article content in sessionstorage
+    // assuming some text was highlighted on the last search
+    if (!sessionStorage.getItem("content")) saveContentInSession();
+    else removeSearchHighlight();
+
+    // check for empty input
+    const input = searchBar.value.trim();
+    if (!input) return;
+
+    // if not empty, search
+    const query = input.replace(/\s+/g, " ").toLowerCase(),
+        tree = document.createTreeWalker(document.getElementById("body"));
+    tree.firstChild();
+
+    // search the query individually in every NODE of "tree" treewalker
+    do {
+        checkForChildAndAct(tree.currentNode)
+    } while (tree.nextSibling());
+
+    // arg node can be elem or text
+    function checkForChildAndAct(node) {
+        // no textnode contain another node
+        // elem node may contain child nodes 
+        if (node.hasChildNodes()) node.childNodes.forEach(checkForChildAndAct);
+        else if (node.nodeType === 3) searchAndHighlight(node);
+    }
+
+    // search the query in the textnode
+    // if present, highlight
+    function searchAndHighlight(textnode) {
+        const content = textnode.textContent,
+            position = content.toLowerCase().indexOf(query);
+        if (position !== -1) {
+            const queryLength = query.length;
+            textnode.splitText(position);
+            textnode = textnode.nextSibling;
+            textnode.splitText(queryLength);
+            const mark = document.createElement("mark");
+            mark.innerText = content.substring(position, position + queryLength);
+            mark.className = "mark";
+            textnode.replaceWith(mark);
+            searchAndHighlight(mark.nextSibling);
+        }
+    }
+}
+
+function saveContentInSession() {
+    const content = document.getElementById("body").innerHTML;
+    sessionStorage.setItem("content", JSON.stringify(content))
+}
+
+function removeSearchHighlight() {
+    // marked are highlighted textnodes by using mark tags
+    const marked = document.getElementsByClassName("mark").length;
+    if (marked) {
+        // instead of removing the mark tags, replace the HTML with the original content
+        const content = JSON.parse(sessionStorage.getItem("content"));
+        document.getElementById("body").innerHTML = content
+    }
+}
+
+function reactToInput() {
+    const input = this.value.trim();
+    if (!input && marked) removeSearchHighlight();
+}
+
 // EVENT listeners
 likeBtn.addEventListener("click", toggleLike);
 commentSectionBtn.addEventListener("click", toggleCommentSection);
 shareBtn.addEventListener("click", () => toggleSharingOptions(true));
 shareBtn.addEventListener("blur", () => toggleSharingOptions(false));
 reportBtn.addEventListener("click", openReportDialog);
+searchBar.addEventListener("blur", reactToInput);
+searchForm.addEventListener("submit", searchQuery);
